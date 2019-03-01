a3ccd6b08bcc344ac84d214ecde4faea9cb72c53
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-03-01 12:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addToCart(item) {
  var price = Math.floor(Math.random() * 100) + 1;
   var itemObject = {}
   itemObject [item] = price
-  cart.push(item_object)
+  cart.push(itemObject)
   console.log(item + " has been added to your cart.")
   return cart
 }
