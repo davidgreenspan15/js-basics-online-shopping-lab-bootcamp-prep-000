33236b332c9c289a668e67ed61c92efb56cd23c4
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-03-01 12:39:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,19 +39,18 @@ function total(t) {
 }
 
 function removeFromCart(item) {
-  var itemInCart = false
-  for(var i = 0; i < cart.length; i++){
-    if (cart[i].hasOwnProperty(item)) {
-      itemInCart = true;
-      cart.splice(i, 1);
+  for (var i = 0; i < cart.length; i++){
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1)
+      return cart
     }
   }
-  if (!itemInCart) {
-    console.log("That item is not in your cart.");
-  }
-  return cart
-}
 
+  if (item != cart[item]) {
+    return "That item is not in your cart."
+  }
+
+}
 function placeOrder(cardNumber) { {
   if (!creditNumber) {
     console.log("We don't have a credit card on file for you to place your order.");
