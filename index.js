754a9f80abf28764de005dde170ead2862b7855e
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-03-01 12:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,12 +51,12 @@ function removeFromCart(item) {
   }
 
 }
-function placeOrder(cardNumber) { 
+function placeOrder(cardNumber,total) { 
     if (typeof(cardNumber) === "undefined"){
     return "Sorry, we don't have a credit card on file for you."
   } else {
     cart = [];
-    return `Your total cost is $${t}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
 
 }
 }
