314637dc3d5f364d99944c5841c014e482dfc9e0
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-03-01 13:13:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function total(total) {
     }
   
   for (var j =0; j<pricePerItemInCart.length; j++) {
-    totalPrice = pricePerItemInCart[j]+=totalPrice
+    totalPrice = pricePerItemInCart[j]+totalPrice
   }
   return totalPrice
 }
