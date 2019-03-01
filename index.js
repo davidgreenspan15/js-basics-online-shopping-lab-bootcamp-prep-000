b1accc94da9fdd6093de4d472a424e6bb2c429a2
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-03-01 12:47:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function removeFromCart(item) {
   }
 
 }
-function placeOrder(cardNumber,total) { 
+function placeOrder(cardNumber) { 
     if (typeof(cardNumber) === "undefined"){
     return "Sorry, we don't have a credit card on file for you."
   } else {
